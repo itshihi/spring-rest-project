6a8030a12d3e083f3ddad0a9796d67e8e36cc421
halee <sleepoem43@gmail.com>
2024-09-04 13:07:03 +0900

    project creation

---
+++ src/test/java/org/example/springrestproject/SpringRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springrestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
